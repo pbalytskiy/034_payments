3de8601f6255a6c05cbfcdc8ebfd5ebfa84a2ce1
Paha <p.balytskiy@gmail.com>
2016-08-27 00:04:03 +1000

    invoice form are done

--- js/script.js
+++ js/script.js
@@ -1,34 +1,49 @@
 function extractNumber() {
-    var typeInvoice = /F\/.+?\/SH/g;
+    var typeInvoice = /F\/[\d,]+\/(0|1)\d\/2\d\d\d\/SH/g;
     var numbersHTML = document.getElementById('number-invoice');
     var numbers = deleteSpaces(numbersHTML.value);
-    var arrInvoicesMonth = numbers.match(typeInvoice);
+    var arrInvoicesInputData = numbers.match(typeInvoice);
 
-    console.log(arrInvoicesMonth);
+    var arrAllInvoices = [];
+    for (var i = 0; i < arrInvoicesInputData.length; i++) {
+        arrAllInvoices.push(createInvoicesForEachMonth(arrInvoicesInputData[i]));
+    }
+
+    createInvoicesListHTML(arrAllInvoices);
 }
+
 
 
 function deleteSpaces(numbers) {
-    return numbers.replace(/ /g, "");
+    return numbers.replace(/\s/g, "");
 }
 
+function createInvoicesForEachMonth(str) {
+    var serial = str.substring(0, 2);
+    var numbersAll = str.slice(2, -11);
+    var monthAndYears = str.slice(-11);
+    var arrNumbersInvoicesForMonth = [];
 
+    if (/,/.test(numbersAll)) {
+        arrNumbersInvoicesForMonth = numbersAll.split(',');
+    }   else arrNumbersInvoicesForMonth.push(numbersAll);
 
-
-function creatInvoiceForEachMonth(str) {
-    var serial = '';
-    var numberInvoice;
-    var monthInvoice;
-    var yearInvoice;
-    var indexInvoice;
-    var serialType = /\//;
-
+    var arrInvoicesForMonth = [];
+    for (var i = 0; i < arrNumbersInvoicesForMonth.length; i++) {
+        var invoice = serial + arrNumbersInvoicesForMonth[i] + monthAndYears;
+        arrInvoicesForMonth.push(invoice);
+    }
+    return arrInvoicesForMonth;
 }
 
-
-//
-// function createArrNumbers(numbers) {
-//     if (/,/.test(numbers)) {
-//         return numbers.split(',')
-//     }
-// }
+function createInvoicesListHTML(arr) {
+    var invoicesListHTML = document.getElementById('output-invoices');
+    invoicesListHTML.innerHTML = ''; // need to discuss
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            var pTeg = document.createElement('p');
+            pTeg.innerHTML = arr[i][j];
+            invoicesListHTML.appendChild(pTeg);
+        }
+    }
+}
